b1c5a18c226f0325a944ee5305a77e7fe5ccb9ac
Arne Blankerts <Arne@Blankerts.de>
2020-12-15 00:34:19 +0100

    Add some more output

--- src/Printer.php
+++ src/Printer.php
@@ -1,7 +1,13 @@
 <?php
 
 class Printer {
+    private string $text;
+    
+    public function __construct(string $text) {
+	$this->text = $text;
+    }
+
     public function print(string $content): void {
-	print $content;
+	print str_replace('%TEXT%', $this->text, $content);
     }
 }
--- src/index.php
+++ src/index.php
@@ -5,5 +5,5 @@ require __DIR__ . '/autoload.php';
 $loader = new Loader(__DIR__ . '/../pages');
 $html = $loader->load('index.html');
 
-$printer = new Printer();
+$printer = new Printer($_SERVER['HTTP_HOST'] . ' @ ' . PHP_VERSION);
 $printer->print($html);
